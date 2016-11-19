2b682f92e9831223d9f421f14a86c7170051c006
JHeadland <jth9@hawaii.edu>
2016-11-19 12:03:07 -1000

    hard mode 1

--- IntroToPygame/interactiveimage.py
+++ IntroToPygame/interactiveimage.py
@@ -1,15 +1,17 @@
 import sys, pygame
+from random import randint, choice
 
 # Initialize Pygame functions
 pygame.init()
 
-# Frames per second setting
+# Settings variables
 FPS = 30
 fpsClock = pygame.time.Clock()
-
-# Screen size variables
 screenX = 800
 screenY = 800
+shapeX = randint(0, screenX)
+shapeY = randint(0, screenY)
+shapeSpeed = [8, 8]
 
 # Create the display surface window
 screen = pygame.display.set_mode((screenX, screenY))
@@ -31,6 +33,7 @@ speed = 10 # Number of pixels the image moves
 while True:
 
 	screen.fill((255, 255, 255)) # Makes the screen background white
+	pygame.draw.circle(screen, (0, 0, 255), (shapeX, shapeY), 20, 0)
 	screen.blit(currentImg, imgRect) # Puts the image onto the display surface
 
 	# The event handling loop
@@ -54,6 +57,27 @@ while True:
 		imgRect.centery -= speed
 	if pressed[pygame.K_DOWN] and imgRect.bottom < screenY:
 		imgRect.centery += speed
+
+	if shapeX < 10 or shapeX >= screenX - 10:
+		shapeSpeed[0] *= -1
+		shapeSpeed[0] += randint(-10, 10)
+	if shapeY < 10 or shapeY >= screenY - 10:
+		shapeSpeed[1] *= -1
+		shapeSpeed[1] += randint(-10, 10)
+	shapeX += shapeSpeed[0]
+	shapeY += shapeSpeed[1]
+
+	if imgRect.collidepoint(shapeX, shapeY):
+		if currentImg == img1:
+			img1 = pygame.transform.scale(img1, (imgRect.width + 5, imgRect.height + 5))
+			currentImg = img1
+		if currentImg == img2:
+			img2 = pygame.transform.scale(img2, (imgRect.width + 5, imgRect.height + 5))
+			currentImg = img2
+		imgRect.inflate_ip(5, 5)
+		shapeX = randint(0, screenX)
+		shapeY = randint(0, screenY)
+		shapeSpeed = [8 * choice([-1, 1]), 8 * choice([-1, 1])]
 
 	pygame.display.flip() # Update and redraw the screen
 	fpsClock.tick(FPS)
